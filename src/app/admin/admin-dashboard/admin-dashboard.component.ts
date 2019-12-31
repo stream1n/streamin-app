ab5abb0aff5be97c196fc6e58548a51a7b015619
Ronan Hughes <streamin@Ronans-MacBook-Pro.local>
2019-12-31 21:50:15 +0000

    results

--- src/app/admin/admin-dashboard/admin-dashboard.component.ts
+++ src/app/admin/admin-dashboard/admin-dashboard.component.ts
@@ -17,20 +17,8 @@ export class AdminDashboardComponent implements OnInit {
   constructor(private auth: AuthService, private http: HttpClient) { }
 
   ngOnInit() {
-
     this.photoUrl$ = this.auth.getUserPhotoURL();
     this.displayName$ = this.auth.getUserDisplayName();
-    this.auth.GetUserToken().then((idToken) => {
-      const httpOptions = {
-        headers: new HttpHeaders({
-          'Content-Type': 'application/json',
-          'Access-Control-Allow-Origin': '*',
-          Authorization: 'Bearer ' + idToken
-        }),
-      };
-      this.http.get(`api/secureendpoint`, httpOptions).subscribe((data) => console.log(data));
-    });
-
   }
 
   logout() {
